98c7bfa00a7d90c0dbd5c95083f3930bcaec5001
activeliang <activeliang@gmail.com>
2017-03-19 13:29:16 +0800

    only admin can access backed panel

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u = User.new
+u.email = "liang@liang.com"
+u.password = "llllll"
+u.password_confirmation = "llllll"
+u.is_admin = true
+u.save
