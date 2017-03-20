b375cf9e6ba10aa7dd750736adbfdd5745abb159
activeliang <activeliang@gmail.com>
2017-03-20 09:55:45 +0800

    change quantity in cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -6,4 +6,16 @@ class CartItemsController < ApplicationController
     @cart_item.destroy
     redirect_to carts_path, alert: "你已将#{@cart_item.product.title}移出购物车！"
   end
+
+  def update
+    @cart_item = current_cart.cart_items.find_by(product_id:params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to :back
+  end
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:quantity)
+
+  end
 end
